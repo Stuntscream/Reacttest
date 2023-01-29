22885c6fe9afd72833618a6540b6321844e11de1
stuntscream <edvard-aasum95@hotmail.com>
2023-01-29 22:39:09 +0100

    29.01.2023 second push

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
         <div className="gpt3__navbar-links">
-          <div className="gpt__navbar-links-logo">
-            <img src={logo} alt="logo" />
+        <div className="gpt3__navbar-links_logo">
+        <img src={logo} />
+          </div>
+          <div className="gpt3__navbar-links_container">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Libary</a></p>
           </div>
         </div>
     </div>
